a3f2f341012e5e5a31189f3cd22269e7770efd85
Renee Nordholm <reneenordholm@gmail.com>
2019-09-05 17:29:24 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,14 +1,6 @@
 class Song < ApplicationRecord
     validates :title, presence: true, uniqueness: true
-    validates :released, presence: true
+    validates :released, inclusion: { in: [true, false] }
     validates :artist_name, presence: true
-    # validate :released_year_specs
-
-    def release_year_specs
-        # Optional if released is false
-        # Must not be blank if released is true
-        # Must be less than or equal to the current year
-    end
-
-
+    validates :release_year, presence: true, if: :released?, numericality: { only_integer: true, less_than_or_equal_to: Date.current.year }
 end
