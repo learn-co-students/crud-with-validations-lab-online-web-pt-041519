16677283945ece65eabf1dc803f53f66bc61216e
aparkening <aaron.parkening@gmail.com>
2019-09-09 12:18:50 -0400

    Remove extra comma in Song model. Add delete button and link to index template.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ApplicationRecord
 
-  validates :title, presence: true, 
+  validates :title, presence: true 
   validates :title, uniqueness: { 
     scope: %i[release_year artist_name],
     message: 'Cannot be repeated by the same artist in the same year' 
