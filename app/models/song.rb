5af058a8266b2a64ce53819c164993e7343cbb6a
lanibon <nguyenlan13@gmail.com>
2019-09-11 04:57:46 -0700

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,13 @@
+class Song < ApplicationRecord
+  validates :title, presence: true
+  validates :title, uniqueness: { scope: %i[release_year artist_name], message: "Cannot be repeated" }
+  validates :released, inclusion: { in: [true, false] }
+  validates :release_year, presence: true, if: :released?
+  validates :release_year, presence: true, numericality: { only_integer: true, less_than_or_equal_to: Date.today.year }, if: :released?
+  validates :artist_name, presence: true
+
+  def released?
+    released
+  end
+
+end
