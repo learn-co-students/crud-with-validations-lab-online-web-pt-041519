1be1ed1a5b2b1fba829c8e340b1abf5542ae4a0f
Dan Sullivan <dansullivan19@gmail.com>
2019-10-24 13:44:41 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,8 @@
+class Song < ApplicationRecord
+    validates :title, presence: true
+    validates :title, uniqueness: true
+    with_options :if => :released? do |song|
+        song.validates :release_year, presence: true
+        song.validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}
+      end
+end
